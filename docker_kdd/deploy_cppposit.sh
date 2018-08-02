e28e8bc6d7d36b7db96e8f8340125616fe04af57
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-08-02 07:55:08 +0200

    finally works on docker

--- docker_kdd/deploy_cppposit.sh
+++ docker_kdd/deploy_cppposit.sh
@@ -5,5 +5,5 @@ git clone --depth 1 https://github.com/eruffaldi/cppPosit.git
 cd cppPosit
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 make install
--- docker_kdd/deploy_softfloat.sh
+++ docker_kdd/deploy_softfloat.sh
@@ -5,5 +5,5 @@ git clone --depth 1 https://github.com/eruffaldi/berkeley-softfloat-3.git
 cd berkeley-softfloat-3
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 make install
